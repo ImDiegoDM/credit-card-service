81494a599e8e7621b2168fd31583d5d8db0ec6ba
Diego Matias <dm.diego.bh@gmail.com>
2018-10-03 19:20:42 -0300

    Feature tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,10 +12,58 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testGetPayment()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/v1/public/pagamentos/1');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertJson([
+            'id' => 1,
+            'user'=>[
+                'id'=>0,
+                'name'=>'Diego Matias de Oliviera',
+                'cpf'=>'111.111.111-11'
+            ],
+            'numero_cartão'=> '1234567891112',
+            'valor'=> '150',
+            'pedidos_id'=> 1
+        ]);
+    }
+
+    public function testPostPaymentSuccess()
+    {
+        $creditCard = [ 
+            'numero_cartão'=> '1234567891112',
+            'valor'=> '150',
+            'pedidos_id'=> 1
+        ];
+
+        $response = $this->withHeaders([
+            'Authorization' => '123',
+        ])->json('POST', '/api/v1/public/pagamentos', $creditCard);
+
+        $response->assertStatus(201)->assertJson([
+            'id' => 1,
+            'user'=>[
+                'id'=>0,
+                'name'=>'Diego Matias de Oliviera',
+                'cpf'=>'111.111.111-11'
+            ],
+            'numero_cartão'=> '1234567891112',
+            'valor'=> '150',
+            'pedidos_id'=> 1
+        ]);
+    }
+
+    public function testPostPaymentWhithoutHeader()
+    {
+        $creditCard = [ 
+            'numero_cartão'=> '1234567891112',
+            'valor'=> '150',
+            'pedidos_id'=> 1
+        ];
+
+        $response = $this->json('POST', '/api/v1/public/pagamentos', $creditCard);
+
+        $response->assertStatus(400)->assertJson(['message'=>'Authorization header is missing']);
     }
 }
